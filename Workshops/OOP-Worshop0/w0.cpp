03f75195f61df786d588faff6dd25742610f4e06
aryan <iram.chowdhury777@gmail.com>
2026-01-15 21:38:21 -0500

    Add files via upload

---
+++ Workshops/OOP-Worshop0/w0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void workshopZero() 
+{
+cout << "#### Worskhop Zero ####" << endl;
+cout << "Subject and Section: " << endl;
+cout << "OOP244 NAA" << endl;
+cout << "Name: " << endl;
+cout << "Aryan Chowdhury" << endl;
+cout << "Student Number: " << endl;
+cout << "138197181" << endl; 
+cout << "Email: " << endl;
+cout << "ichowdhury6@myseneca.ca" << endl;
+}
+
+
+int main()
+{
+workshopZero();
+
+return 0;
+}
